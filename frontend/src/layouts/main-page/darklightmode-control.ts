163f9abd79377322196b296cf5cd85738a6fd4ca
gabriel remes <gabm.remes@gmail.com>
2024-01-24 12:50:48 -0300

    FIX: arreglo modo claro-oscuro

--- frontend/src/layouts/main-page/darklightmode-control.ts
+++ frontend/src/layouts/main-page/darklightmode-control.ts
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.getElementById("toggleButton") as HTMLButtonElement;
-  const body = document.getElementById("body_layout") as HTMLBodyElement;
+  const body = document.body as HTMLBodyElement;
 
   const darkModeClasses = ['bg-gray-900', 'text-white'];
   const lightModeClasses = ['bg-gray-100', 'text-black'];
 
   toggleButton.addEventListener("click", () => {
-    if (darkModeClasses.every(cls => body.classList.contains(cls))) {
+    if (darkModeClasses.every(cls => body?.classList?.contains(cls))) {
       setTimeout(()=>{
-        lightModeClasses.forEach(cls => body.classList.add(cls));
-      darkModeClasses.forEach(cls => body.classList.remove(cls));
+        lightModeClasses.forEach(cls => body?.classList?.add(cls));
+      darkModeClasses.forEach(cls => body?.classList?.remove(cls));
 
       }, 50);
       
     } else {
       setTimeout(()=>{
-        darkModeClasses.forEach(cls => body.classList.add(cls));
-        lightModeClasses.forEach(cls => body.classList.remove(cls));
+        darkModeClasses.forEach(cls => body?.classList?.add(cls));
+        lightModeClasses.forEach(cls => body?.classList?.remove(cls));
 
       }, 50);
      
