a87baac0f2c73b7a011c2d27303ef51ff370b8ab
gabriel remes <gabm.remes@gmail.com>
2024-03-28 12:42:20 -0300

    dist

--- .netlify/functions-internal/ssr/chunks/lumini_D2lHiGTy.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/lumini_CdXUxf71.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/tripmate_BSfl-owA.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/tripmate_C-t1uBZC.mjs');
-
-export { page };
